07d278e72f21bd41b7d193c0b0fe3a065069c319
gkjt <gkjt@users.noreply.github.com>
2015-01-07 12:20:55 +0000

    New Controller Version Start

---
+++ Assets/ControllerV5/ControllerV5.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ControllerV4 : MonoBehaviour {
+    
+    public void Start(){
+        
+    }
+    
+    public void Update(){
+        
+    }
+    
+    public void setThrottles(int a, int b, int c, int d){
+    
+    }
+
+}
